140f3ecdef76d677eea233ef6944efa045eefad9
dpxrk <danielpark0503@gmail.com>
2024-07-25 17:03:01 -0400

    BST

---
+++ BST/BSTPrint.java
@@ -0,0 +1,19 @@
+package BST;
+
+public class BSTPrint {
+  
+  
+
+//   public void BSTPrintInOrder(Node node){
+//     if (node == null){
+//       return;
+//     }
+
+//     BSTPrintInOrder(node.left);
+//     System.out.println("Node: " + node.value);
+//     BSTPrintInOrder(node.right);
+// }
+  public static void main(String args[]){
+
+  }
+}
---
+++ BST/Node.java
@@ -0,0 +1,41 @@
+package BST;
+
+public class Node {
+
+  private int value;
+  private Node left;
+  private Node right;
+
+  public Node(int value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+
+  }
+
+  public int getValue(){
+    return value;
+  }
+  public void setValue(int value) {
+    this.value = value;
+}
+
+public Node getLeft() {
+    return left;
+}
+
+public void setLeft(Node left) {
+    this.left = left;
+}
+
+public Node getRight() {
+    return right;
+}
+
+public void setRight(Node right) {
+    this.right = right;
+}
+
+
+  
+}
